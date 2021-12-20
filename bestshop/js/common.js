8965b5c6c1035ccff090981efbe84efd0f6e7cd3
water452 <46914829+water452@users.noreply.github.com>
2021-12-20 16:11:49 +0900

    [이수용] 폴더구조 업데이트

--- bestshop/js/common.js
+++ bestshop/js/common.js
@@ -1,94 +1,106 @@
-$(document).ready(function() {
-  var _winW = $(window).width();
+var _thsW = $(window).width();
 
-  // click layers
-  $(function(){
-    $('.layer-click > a').on('click', function(e){
-      if(e.type == 'mouseenter'){}
-      $(this).toggleClass('active').closest('.layer-click').find('.layer-detail').toggle();
-    });
+// click layers
+function clickLayer(){
+  $('.layer-click > a').on('click', function(e){
+    if(e.type == 'mouseenter'){}
+    $(this).toggleClass('active').closest('.layer-click').find('.layer-detail').toggle();
+  });
+}
+
+// MEGA GNB
+function megaGnb(){ 
+  $('.pc .menu-wrap > ul > li').on('mouseenter mouseleave', function(e){
+    if(e.type == 'mouseenter'){
+      $('.menu-wrap > ul > li').removeClass('active');
+      $('.menu-wrap .dimmde').remove();
+      $('.menu-wrap').append('<p class="dimmde"></p>');
+      $(this).addClass('active');
+    } else if (e.type == 'mouseleave'){
+      $('.menu-wrap .dimmde').remove();
+      $(this).removeClass('active');
+    }
+  });
+}
+
+// focus
+function focusGnb(){
+  // focus > mega gnb
+  $('.pc .menu-wrap > ul > li > a').on('focus', function(){
+    $('.menu-wrap > ul > li').removeClass('active');
+    $('.menu-wrap .dimmde').remove();
+    $('.menu-wrap').append('<p class="dimmde"></p>');
+    $(this).closest('li').addClass('active');
   });
 
-  if(_winW > 767){
-    // my tooltip
-    $(function(){
-      $('.layer-wrap').on('mouseenter mouseleave', function(e){
-        if(e.type == 'mouseenter'){
-          $('.layer-detail').removeClass('active');
-          $(this).find('.layer-detail').addClass('active');
-        } else if (e.type == 'mouseleave'){
-          $(this).find('.layer-detail').removeClass('active')
-        }
-      });
-    });
+  // focus > layer (tool-tip + util)
+  $('.layer-wrap > a').on('focus', function(){
+    $('.layer-detail').removeClass('active');
+    $(this).siblings('.layer-detail').addClass('active');
+  });
 
-    // GNB
-    $(function(){
-      $('.menu-wrap > ul > li').on('mouseenter mouseleave', function(e){
-        if(e.type == 'mouseenter'){
-          $('.menu-wrap > ul > li').removeClass('active');
-          $('.menu-wrap .dimmde').remove();
-          $('.menu-wrap').append('<p class="dimmde"></p>');
-          $(this).addClass('active');
-        } else if (e.type == 'mouseleave'){
-          $('.menu-wrap .dimmde').remove();
-          $(this).removeClass('active');
-        }
-      });
-    });
+  // focus > focusout 
+  $('.wrap a').on('focus', function(){
+    if(!$(this).parents('div').hasClass('menu-wrap')){ // mega-gnb
+      $('.menu-wrap .dimmde').remove();
+      $('.menu-wrap > ul > li').removeClass('active');
+    }
+    if(!$(this).parents('div').hasClass('my-tooltip')){ // tooltip
+      $('.my-tooltip .layer-detail').removeClass('active');
+    }
+    if(!$(this).parents('div').hasClass('about-company')){ // util
+      $('.about-company .layer-detail').removeClass('active');
+    }
+  });
+}
 
-    // focus
-    $(function(){
-      // focus > mega gnb
-      $('.menu-wrap > ul > li > a').on('focus', function(){
-        $('.menu-wrap > ul > li').removeClass('active');
-        $('.menu-wrap .dimmde').remove();
-        $('.menu-wrap').append('<p class="dimmde"></p>');
-        $(this).closest('li').addClass('active');
-      });
+// my tooltip
+function myTooltip(){
+  $('.layer-wrap').on('mouseenter mouseleave', function(e){
+    if(e.type == 'mouseenter'){
+      $('.layer-detail').removeClass('active');
+      $(this).find('.layer-detail').addClass('active');
+    } else if (e.type == 'mouseleave'){
+      $(this).find('.layer-detail').removeClass('active')
+    }
+  });
+}
 
-      // focus > layer (tool-tip + util)
-      $('.layer-wrap > a').on('focus', function(){
-        $('.layer-detail').removeClass('active');
-        $(this).siblings('.layer-detail').addClass('active');
-      });
+// FOOTER GNB
+function footerGnb(){
+  $('footer .depth1 a').click(function(){
+    $(this).closest('.depth1').toggleClass('active').siblings('.depth2').stop().slideToggle(200);
+  });
+  $('footer .depth3-tit').click(function(){
+    $(this).toggleClass('active').siblings('ul').stop().slideToggle(200);
+  });
+}
 
-      // focus > focusout 
-      $('.wrap a').on('focus', function(){
-        if(!$(this).parents('div').hasClass('menu-wrap')){ // mega-gnb
-          $('.menu-wrap .dimmde').remove();
-          $('.menu-wrap > ul > li').removeClass('active');
-        }
-        if(!$(this).parents('div').hasClass('my-tooltip')){ // tooltip
-          $('.my-tooltip .layer-detail').removeClass('active');
-        }
-        if(!$(this).parents('div').hasClass('about-company')){ // util
-          $('.about-company .layer-detail').removeClass('active');
-        }
-      });
-    });
+function footMenu(){
+  // footer 메뉴 전체보기 버튼
+  var footerMenu = $('.pc-dropdown-wrap'),
+      footerMenuBtn = $('.pc-dropdown-wrap .btns .menu-opener')
 
-    // footer 메뉴 전체보기 버튼
-    var footerMenu = $('footer .pc-dropdown-wrap');
-    var footerMenuBtn = $('footer .pc-dropdown-wrap .btns .menu-opener');
-  
-    footerMenuBtn.on('click', function() {
-      if(footerMenu.hasClass('open')){
-        footerMenu.removeClass('open')
-        footerMenuBtn.children('span').text('메뉴 전체보기')
-      } else {
-        footerMenu.addClass('open');
-        footerMenuBtn.children('span').text('메뉴 접기')
-      }
-    });
-  }
-  if(_winW <= 767){
-    $('footer .depth1 a').click(function(){
-      $(this).closest('.depth1').toggleClass('active').siblings('.depth2').stop().slideToggle(200);
-    });
-    $('footer .depth3-tit').click(function(){
-      $(this).toggleClass('active').siblings('ul').stop().slideToggle(200);
-    });
+  footerMenuBtn.on('click', function() {
+    footerMenu.toggleClass('open')
+    if(footerMenu.hasClass('open')){
+      footerMenuBtn.children('span').text('메뉴 접기')
+    } else {
+      footerMenuBtn.children('span').text('메뉴 전체보기')
+    }
+  });
+}
+
+$(document).ready(function() {
+  clickLayer();
+  footMenu();
+
+  if(_thsW > 767){
+    megaGnb();
+    myTooltip();
+    focusGnb();
+  } else { // mobile size
+    footerGnb();
   }
 
   //mega-gnb > banner
@@ -106,5 +118,7 @@ $(document).ready(function() {
 });
 
 $(window).resize(function(){
-  var _winW = $(window).width();
+  var _thsW = $(window).width();
+
+  console.log(_thsW);
 });
---
+++ bestshop/pages/information/index.jsp
@@ -0,0 +1,4 @@
+<%@ page contentType="text/html; charset=utf-8" %>
+<jsp:include page="./templates/common/header.jsp" />
+<div id="content"></div>
+<jsp:include page="./templates/common/footer.jsp" />
